20f9a93f9b8308b01801daa8ec50678eb81e9d6e
phupwintkai <phupwintkhaing@mm.cybermissions.co.jp>
2024-06-18 15:59:17 +0630

    all unread message api の修正

---
+++ db/migrate/20240618065229_add_unread_thread_message_columns_to_t_user_channel.rb
@@ -0,0 +1,8 @@
+class AddUnreadThreadMessageColumnsToTUserChannel < ActiveRecord::Migration[7.1]
+  def up
+    add_column :t_user_channels, :unread_thread_message, :string
+  end
+  def down
+    remove_column :t_user_channels, :unread_thread_message, :string
+  end
+end
